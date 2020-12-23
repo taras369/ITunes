82b2d79736bcd5ac249bc4ed9a62024bf6767a32
Taras <taras369ky@gmail.com>
2020-12-23 13:42:05 +0200

    Radio Player added

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,66 @@
 export const radioPlayerInit = () => {
-    console.log("RadioInit");
+
+    const radio = document.querySelector('.radio');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioHeaderBig = document.querySelector('.radio-header__big');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio();  
+    audio.type = 'audio/aac'; 
+
+    radioStop.disabled = true;
+
+    //Pause/Play button
+    const changeIconPlay = () => {
+        if(audio.paused) {
+            radio.classList.remove('play');
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop');
+        } else {
+            radio.classList.add('play');
+            radioStop.classList.add('fa-stop');
+            radioStop.classList.remove('fa-play');
+        }
+    };
+
+    const selectItem = elem => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        elem.classList.add('select');
+    };
+
+    //Radio name+img changed, added gray ring
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        const parrent = target.closest('.radio-item');
+        selectItem(parrent);
+
+        const title = parrent.querySelector('.radio-name').textContent;
+        radioHeaderBig.textContent = title;
+
+        const img = parrent.querySelector('.radio-img').src;
+        radioCoverImg.src = img;
+
+
+        radioStop.disabled = false;
+        audio.src = target.dataset.radioStantion;
+        audio.play();
+        changeIconPlay();
+    });
+
+
+    //Pause/Play radio
+    radioStop.addEventListener('click', () => {
+        if(audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+        changeIconPlay();
+    });
+
+
+
+
 };
